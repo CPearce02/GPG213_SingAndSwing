69b6fbf3663683c2cc0fdaf1784c773a723150ed
Elliot <ellioteweb@gmail.com>
2023-02-18 19:06:24 +1100

    Section handler

---
+++ Assets/SectionHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SegmentHandler : MonoBehaviour
+{
+    [field:SerializeField] public CinemachineVirtualCamera cam { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
