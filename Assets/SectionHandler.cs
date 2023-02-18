ea2350321674c2842c2ce2c83057a1ef6ebc5ce4
Elliot <ellioteweb@gmail.com>
2023-02-18 19:07:28 +1100

    Added awake

--- Assets/SectionHandler.cs
+++ Assets/SectionHandler.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class SegmentHandler : MonoBehaviour
 {
     [field:SerializeField] public CinemachineVirtualCamera VCam { get; private set; }
+
+    private void Awake()
+    {
+        VCam = GetComponent<CinemachineVirtualCamera>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
