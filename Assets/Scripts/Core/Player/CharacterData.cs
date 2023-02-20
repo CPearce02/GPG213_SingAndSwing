e78aa7cc3cbd97d38aef151c4aad7f9d89c90bb2
Elliot <ellioteweb@gmail.com>
2023-02-20 18:26:56 +1100

    ChangeHealth function

--- Assets/Scripts/Core/Player/CharacterData.cs
+++ Assets/Scripts/Core/Player/CharacterData.cs
@@ -7,19 +7,23 @@ namespace Core.Player
     [CreateAssetMenu(fileName = "_Character", menuName = "Characters/New Character", order = 0)]
     public class CharacterData : ScriptableObject
     {
+        
         [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public int Health { get; private set; }
         [field: SerializeField] public int Mana { get; private set; }
         [field: SerializeField] public float MoveSpeed { get; private set; }
-
         [field: SerializeField] public float JumpSpeed { get; private set; }
-
         [field: SerializeField] public float JumpHeight { get; private set; }
 
         [SerializeField] private List<AbilityData> abilities = new();
 
         public bool CanMove { get; private set; }
         public bool CanAttack { get; private set; }
-        
+
+        public void ChangeHealth(int amount)
+        {
+            Health += amount;
+        }
+
     }
 }
