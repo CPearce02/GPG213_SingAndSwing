747e2ece7b27679211f747f49c70fa5cae2a62bd
Elliot <ellioteweb@gmail.com>
2023-02-23 18:08:04 +1100

    added URP

--- Assets/Scripts/Levels/ScriptableObjects/Sections/BattleSectionData.cs
+++ Assets/Scripts/Levels/ScriptableObjects/Sections/BattleSectionData.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Levels.ScriptableObjects.Sections
 {
     [CreateAssetMenu(fileName = "BattleSectionData", menuName = "Levels/Sections/New Battle Section Data", order = 0)]
     public class BattleSectionData : LevelSectionData
     {
-        [field:SerializeField] public Enemy EnemyToBattle { get; private set; }
+        [field:SerializeField] public List<Enemy> Enemies { get; private set; }
     }
 }
