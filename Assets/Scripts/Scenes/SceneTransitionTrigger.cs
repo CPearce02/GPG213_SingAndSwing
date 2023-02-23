e282a0a64bd37ca2a3d9829a267251d4ef4bdab6
Elliot <ellioteweb@gmail.com>
2023-02-23 22:34:59 +1100

    Scene trigger uses temp string

--- Assets/Scripts/Scenes/SceneTransitionTrigger.cs
+++ Assets/Scripts/Scenes/SceneTransitionTrigger.cs
@@ -1,17 +1,22 @@
 using Events;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Scenes
 {
     public class SceneTransitionTrigger : MonoBehaviour
     {
+        [SerializeField] private string levelToLoad;
 
         private void OnTriggerEnter2D(Collider2D col)
         {
             col.TryGetComponent(out PlatformingController player);
-            
-            if(player)
-                GameEvents.onSceneTransitionOutEvent?.Invoke();
+
+            if (player)
+            {
+                SceneManager.LoadScene(levelToLoad);
+            // GameEvents.onSceneTransitionOutEvent?.Invoke();
+            }
         }
     }
 }
--- Assets/Scripts/Scenes/TransitionCanvas.cs
+++ Assets/Scripts/Scenes/TransitionCanvas.cs
@@ -1,6 +1,5 @@
 using Events;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Scenes
 {
