5aa6608fe357d47a5849a45527ef2f9c16620ebc
RockMuncher7 <106937171+RockMuncher7@users.noreply.github.com>
2023-02-23 21:33:53 +1100

    Prototype Main Menu added

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadLevel(string sceneName) => SceneManager.LoadScene(sceneName);
+    public void Quit() => Application.Quit();
+}
