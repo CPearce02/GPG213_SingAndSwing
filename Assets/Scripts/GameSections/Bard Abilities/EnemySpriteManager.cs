edaa98a41ac66a3d30f9f331ea1aaee60c309dfe
CPearce02 <107232375+CPearce02@users.noreply.github.com>
2023-03-02 22:47:58 +1100

    ColorUpdates

---
+++ Assets/Scripts/GameSections/Bard Abilities/EnemySpriteManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpriteManager : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private EnemyPlatforming ep;
+    //private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ep = GetComponentInParent<EnemyPlatforming>();
+        //originalColor = sr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ep.canBeDestroyed)
+        {
+            sr.color = Color.red;
+        }
+        else
+        {
+            sr.color = Color.cyan;
+        }
+    }
+}
