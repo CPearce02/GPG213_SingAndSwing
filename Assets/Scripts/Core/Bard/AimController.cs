38029889bf4d9e1041856a66ffc74e82a3dfe829
CPearce02 <107232375+CPearce02@users.noreply.github.com>
2023-03-09 14:15:28 +1100

    bardnojump

--- Assets/Scripts/Core/Bard/AimController.cs
+++ Assets/Scripts/Core/Bard/AimController.cs
@@ -2,31 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Events;
+using UnityEngine.InputSystem;
 
 public class AimController : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 2f;
     private Vector2 aimDirection;
     private Collider2D aimCollider;
+    [SerializeField] private PlayerInput _bardInput;
 
-    private void OnEnable()
-    {
-        GameEvents.onAimStart += AimTowards;
-    }
+    //private void OnEnable()
+    //{
+    //    GameEvents.onAimStart += AimTowards;
+    //}
 
-    private void OnDisable()
-    {
-        GameEvents.onAimStart -= AimTowards;
-    }
+    //private void OnDisable()
+    //{
+    //    GameEvents.onAimStart -= AimTowards;
+    //}
 
-    private void AimTowards(Vector2 direction)
-    {
-        aimDirection = direction;
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+    //private void AimTowards(Vector2 direction)
+    //{
+    //    aimDirection = direction.normalized;
+    //    float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
 
-        //Rotate the target indicator towards the mouse position
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
-    }
+    //    //Rotate the target indicator towards the mouse position
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
+    //}
     // Start is called before the first frame update
     void Start()
     {
@@ -38,9 +40,11 @@ public class AimController : MonoBehaviour
     {
         ////Vector2 aimDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
 
-        //float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimDirection = _bardInput.actions["Aim"].ReadValue<Vector2>();
 
-        //// Rotate the target indicator towards the mouse position
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+        // Rotate the target indicator towards the mouse position
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
     }
 }
