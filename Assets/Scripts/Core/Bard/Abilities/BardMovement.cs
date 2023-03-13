543be7e7cdae9c3efd4a7153d0c1c6c3ef0c0044
RockMuncher7 <106937171+RockMuncher7@users.noreply.github.com>
2023-03-13 18:59:20 +1100

    Much smoother bard movement

--- Assets/Scripts/Core/Bard/Abilities/BardMovement.cs
+++ Assets/Scripts/Core/Bard/Abilities/BardMovement.cs
@@ -7,16 +7,15 @@ using System;
 public class BardMovement : MonoBehaviour
 {
     PlatformingController knightController;
-    private Transform _playerTransform;
-    private float _speed = 10f;
-    public float followRange;
+    public Transform followObj;
+    public float speed = 10f;
+    public float followRange = 0.5f;
+    public float relocateSpeed = 15f, relocateRange = 3f;
     private Rigidbody2D _rb;
-    float _distance;
-    Vector2 direction, newPosition;
+    float _distanceX, _distance;
 
     private void Start()
     {
-        _playerTransform = GameObject.Find("Follow").transform;
         _rb = GetComponent<Rigidbody2D>();
     }
 
@@ -33,21 +32,22 @@ public class BardMovement : MonoBehaviour
 
     private void Update()
     {
-        _distance = Vector2.Distance(transform.position, _playerTransform.position);
+        _distance = Vector2.Distance(followObj.position, transform.position);
+        _distanceX = followObj.position.x - transform.position.x;
 
-        direction = (_playerTransform.position - transform.position).normalized;
+        CheckRelocate();
     }
 
     private void FixedUpdate()
     {
-        if (_distance > followRange)
-        {
-            newPosition = _rb.position + direction * _speed * Time.fixedDeltaTime;
-            _rb.MovePosition(newPosition);
-        } else
-        {
-            _rb.velocity = new Vector2(0, _rb.velocity.y);
-        }
+        MoveBard();
+    }
+
+    void MoveBard()
+    {
+        if (_distanceX > followRange || _distanceX < -followRange)
+            _rb.AddForce(new Vector2(_distanceX * speed * Time.fixedDeltaTime, 0));
+        else if (knightController.Grounded) _rb.velocity = new Vector2(_rb.velocity.x / 1.5f, _rb.velocity.y);
     }
 
     public void BardJump()
@@ -56,9 +56,19 @@ public class BardMovement : MonoBehaviour
         _rb.AddForce(transform.up * knightController.JumpSpeed, ForceMode2D.Impulse);
     }
 
+    void CheckRelocate()
+    {
+        if (_distance > relocateRange) RelocateBard();
+    }
+
+    void RelocateBard()
+    {
+        transform.position = followObj.position;
+        _rb.velocity = Vector2.zero;
+    }
+
     private void SetPlayer(PlatformingController player)
     {
-        _playerTransform = player.FollowTransform;
         knightController = player;
 
         knightController.JumpEvent += BardJump;
