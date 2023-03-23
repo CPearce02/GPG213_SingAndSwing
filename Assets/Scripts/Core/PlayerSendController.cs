1eb798de734d868d7b4c2f2732db6ad15d30c6ff
Elliot <ellioteweb@gmail.com>
2023-03-23 16:29:22 +1100

    organisation

---
+++ Assets/Scripts/Core/PlayerSendController.cs
@@ -0,0 +1,32 @@
+using Core.Player;
+using Events;
+using UnityEngine;
+
+namespace Core
+{
+    public class PlayerSendController : MonoBehaviour
+    {
+
+        [SerializeField][ReadOnly] PlatformingController player;
+
+        private void Awake()
+        {
+            player = gameObject.GetComponent<PlatformingController>();
+        }
+
+        private void OnEnable()
+        {
+            GameEvents.onRequestPlayerEvent += SendPlayer;
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.onRequestPlayerEvent -= SendPlayer;
+        }
+
+        // We might need to refactor when we have two players
+
+        private void SendPlayer() => GameEvents.onSendPlayerEvent?.Invoke(player);
+
+    }
+}
---
+++ Assets/Scripts/Effects/ShieldHandler.cs
@@ -0,0 +1,43 @@
+using Enemies;
+using Structs;
+using UnityEngine;
+
+namespace Effects
+{
+    public class ShieldHandler : MonoBehaviour
+    {
+        SpriteRenderer _spriteRenderer;
+        [SerializeField] Enemy enemy;
+        [Header("Material Settings")]
+        [SerializeField] Material defaultMaterial;
+        [SerializeField] Material shieldMaterial;
+        [Header("Particle Settings")]
+        [SerializeField] ParticleEvent particleEvent;
+    
+        private void Awake()
+        {
+            if(enemy == null) enemy = GetComponentInParent<Enemy>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            if (enemy == null) return;
+            enemy.Destroyable += SetMaterial;
+        }
+
+        private void OnDisable()
+        {
+            if (enemy == null) return;
+            enemy.Destroyable -= SetMaterial;
+        }
+    
+        void SetMaterial(bool canBeDestroyed)
+        {
+            _spriteRenderer.material = canBeDestroyed ? defaultMaterial : shieldMaterial;
+
+            if(canBeDestroyed)
+                particleEvent.Invoke();
+        }
+    }
+}
