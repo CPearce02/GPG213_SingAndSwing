1a2388caec5e54d750c6c6142bbea023c15f3968
Elliot <ellioteweb@gmail.com>
2023-03-27 18:30:13 +1100

    enemyboss state machine comment

--- Assets/Scripts/Enemies/EnemyStateMachine.cs
+++ Assets/Scripts/Enemies/EnemyStateMachine.cs
@@ -19,6 +19,8 @@ namespace Enemies
         void Start()
         {
             Rb = GetComponent<Rigidbody2D>();
+            
+            // if enemyData.type is Boss, then change state to BossIdleState else change state to IdleState
             ChangeState(new IdleState());
         }
     
