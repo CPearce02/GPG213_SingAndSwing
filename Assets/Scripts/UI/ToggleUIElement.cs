cdf3dfa17c91752adfcd3e9436e953c09f11ec54
Elliot <ellioteweb@gmail.com>
2023-03-27 19:47:34 +1100

    kill handling for dotween

--- Assets/Scripts/UI/ToggleUIElement.cs
+++ Assets/Scripts/UI/ToggleUIElement.cs
@@ -10,7 +10,12 @@ namespace UI
         [SerializeField] InputAction button;
         [SerializeField] private float animationDuration = .2f;
         [SerializeField] private float scaleFactor = 1.1f;
-        
+        private Tween _fadeIn;
+        private Tween _fadeOut;
+        private Tween _scale;
+        private Tween _resetScale;
+        private Tween _fadeAndScale;
+  
         private void OnEnable()
         {
             button.Enable();
@@ -21,22 +26,23 @@ namespace UI
         {
             button.Disable();
             button.performed -= Toggle;
+            DOTween.KillAll();
         }
 
         void Toggle(InputAction.CallbackContext context)
         {
             if (uiPrefab.activeInHierarchy)
             {
-                Tween fade = uiPrefab.GetComponent<CanvasGroup>().DOFade(0, animationDuration);
-                Tween scale = uiPrefab.transform.DOScale(Vector3.one / scaleFactor, animationDuration);
-                Tween fadeAndScale = DOTween.Sequence().Append(fade).Join(scale);
-                fadeAndScale.OnComplete(() => { uiPrefab.SetActive(false); });
+                _fadeIn = uiPrefab.GetComponent<CanvasGroup>().DOFade(0, animationDuration);
+                _scale = uiPrefab.transform.DOScale(Vector3.one / scaleFactor, animationDuration);
+                _fadeAndScale = DOTween.Sequence().Append(_fadeIn).Join(_scale);
+                _fadeAndScale.OnComplete(() => { uiPrefab.SetActive(false); });
             }
             else
             {
                 uiPrefab.transform.localScale = Vector3.one / scaleFactor;
                 uiPrefab.GetComponent<CanvasGroup>().DOFade(1, animationDuration);
-                uiPrefab.transform.DOScale(Vector3.one, animationDuration);
+                _fadeOut = uiPrefab.transform.DOScale(Vector3.one, animationDuration);
                 uiPrefab.SetActive(true);
             }
         }
