7d242936f9c4526f001686e2716c6dd78d3a2f38
CPearce02 <107232375+CPearce02@users.noreply.github.com>
2023-03-29 18:05:37 +1100

    BossStart

---
+++ Assets/Scripts/Enemies/BossStates/BossAimState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interfaces;
+
+namespace Enemies.BossStates
+{
+    public class BossAimState : IState
+    {
+        public void Enter(EnemyStateMachine enemy)
+        {
+        }
+
+        public void Execute(EnemyStateMachine enemy)
+        {
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
+
---
+++ Assets/Scripts/Enemies/BossStates/BossIdleState.cs
@@ -0,0 +1,31 @@
+using Interfaces;
+using UnityEngine;
+using Core.Player;
+
+namespace Enemies.BossStates
+{
+    public class BossIdleState : IState
+    {
+        private EnemyStateMachine _enemy;
+
+        public void Enter(EnemyStateMachine enemy)
+        {
+            this._enemy = enemy;
+        }
+
+        public void Execute(EnemyStateMachine enemy)
+        {
+            var hit = Physics2D.OverlapCircle(enemy.transform.position, enemy.enemyData.triggerRange, enemy.PlayerLayer);
+            if (hit != null && hit.TryGetComponent(out PlatformingController player))
+            {
+                _enemy.ChangeState(new BossAimState());
+            }
+        }
+
+        public void Exit()
+        {
+
+        }
+    }
+}
+
