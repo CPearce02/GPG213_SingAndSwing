96dd75dccaa5cdc7b6ae8adc239efb14d68ecc3c
Elliot <ellioteweb@gmail.com>
2023-04-07 12:53:52 +1000

    Updates to level 1

--- Assets/Scripts/Scenes/SceneTransitionTrigger.cs
+++ Assets/Scripts/Scenes/SceneTransitionTrigger.cs
@@ -7,6 +7,9 @@ namespace Scenes
 {
     public class SceneTransitionTrigger : MonoBehaviour
     {
+        private BoxCollider2D boxCollider;
+        
+        
         private void OnTriggerEnter2D(Collider2D col)
         {
             col.TryGetComponent(out PlatformingController player);
@@ -25,9 +28,10 @@ namespace Scenes
 
         private void OnDrawGizmos()
         {
+            boxCollider = GetComponent<BoxCollider2D>();
             Gizmos.color = new Color(0, 1, 0, 0.25f);
             var t = transform;
-            Gizmos.DrawCube(t.position, t.localScale);
+            Gizmos.DrawCube(t.position, boxCollider.size);
         }
     }
 }
