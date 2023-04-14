bf837526087a4858f50e6f14d5d05c795380fc11
Elliot <ellioteweb@gmail.com>
2023-04-14 16:01:13 +1000

    Updated boss death so that it doesn't loop

--- Assets/Scripts/Enemies/BossStates/BossDeathState.cs
+++ Assets/Scripts/Enemies/BossStates/BossDeathState.cs
@@ -6,8 +6,20 @@ namespace Enemies.BossStates
 {
     public class BossDeathState : IState
     {
+        private BossEnemyStateMachine _enemy;
+        private SpriteRenderer _bossSpriteRenderer;
+        private Collider2D _bossCollider;
+
+
         public void Enter(EnemyStateMachine enemy)
         {
+            this._enemy = enemy as BossEnemyStateMachine;
+            if (_enemy == null) return;
+
+            _bossSpriteRenderer = _enemy.SpriteRenderer;
+            _bossCollider = _enemy.MainCollider;
+
+            GameEvents.onBossFightEndEvent?.Invoke();
         }
 
         public void Execute(EnemyStateMachine enemy)
@@ -18,7 +30,7 @@ namespace Enemies.BossStates
 
         public void Exit()
         {
-            GameEvents.onBossFightEndEvent?.Invoke();
+
         }
     }
 }
