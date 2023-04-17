5aea9ca1f17ec4aa5b4a7f990d5fb462d23a7180
Elliot <ellioteweb@gmail.com>
2023-04-17 16:15:42 +1000

    scene updates

--- Assets/Scripts/Enemies/SpawnEnemies.cs
+++ Assets/Scripts/Enemies/SpawnEnemies.cs
@@ -1,34 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using Enemies;
-using UnityEngine.Serialization;
 using Enemies.EnemyStates;
+using UnityEngine;
 
-public class SpawnEnemies : MonoBehaviour
+namespace Enemies
 {
-   [SerializeField] private GameObject _enemyPrefab;
-
-    private List<GameObject> spawnedEnemies = new();
-
-    private int spawnLimit = 3;
-
-    public void SetSpawnAmount(int value) => spawnLimit = value;
-
-    public void StartSpawning(Transform target) => StartCoroutine(SpawnEnemy(target));
-    
-    IEnumerator SpawnEnemy(Transform target)
+    public class SpawnEnemies : MonoBehaviour
     {
-        while(spawnedEnemies.Count < spawnLimit)
-        {
-            yield return new WaitForSeconds(1f);
-            var enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
-            spawnedEnemies.Add(enemy);
-            enemy.GetComponent<EnemyStateMachine>().ChangeState(new ChaseState(target));
+        [SerializeField] private GameObject enemyPrefab;
 
-            if(spawnedEnemies.Count >= spawnLimit)
-            {   
-                yield break;
+        private List<GameObject> spawnedEnemies = new();
+
+        [SerializeField] int spawnLimit = 3;
+
+        public void SetSpawnAmount(int value) => spawnLimit = value;
+
+        public void StartSpawning(Transform target) => StartCoroutine(SpawnEnemy(target));
+    
+        IEnumerator SpawnEnemy(Transform target)
+        {
+            while(spawnedEnemies.Count < spawnLimit)
+            {
+                yield return new WaitForSeconds(1f);
+                var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+                spawnedEnemies.Add(enemy);
+                enemy.GetComponent<EnemyStateMachine>().ChangeState(new ChaseState(target));
+
+                if(spawnedEnemies.Count >= spawnLimit)
+                {   
+                    yield break;
+                }
             }
         }
     }
