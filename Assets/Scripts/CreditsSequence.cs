ec26900fc51eab0699a590a3085ed1572386e0c4
RockMuncher7 <106937171+RockMuncher7@users.noreply.github.com>
2023-04-27 15:50:53 +1000

    Credits goes to main menu now

--- Assets/Scripts/CreditsSequence.cs
+++ Assets/Scripts/CreditsSequence.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsSequence : MonoBehaviour
 {
-    [SerializeField] float speed = 100f, initialWait = 3f;
+    [SerializeField] float speed = 100f, initialWait = 3f, creditsTime = 30f;
     bool _creditsStarted = false;
 
     private void Start()
@@ -17,6 +18,8 @@ public class CreditsSequence : MonoBehaviour
         if(_creditsStarted)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+
+            if (Input.anyKeyDown == true) QuitCredits();
         }
     }
 
@@ -24,5 +27,13 @@ public class CreditsSequence : MonoBehaviour
     {
         yield return new WaitForSeconds(initialWait);
         _creditsStarted = true;
+        yield return new WaitForSeconds(creditsTime);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void QuitCredits()
+    {
+        StopCoroutine(WaitForLogo());
+        SceneManager.LoadScene("MainMenu");
     }
 }
