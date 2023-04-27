71a55f209b2cfc01adaf8dd31b268c65537bb4c3
RockMuncher7 <106937171+RockMuncher7@users.noreply.github.com>
2023-04-27 17:10:36 +1000

    Added attack sound

--- Assets/Scripts/Core/Attack.cs
+++ Assets/Scripts/Core/Attack.cs
@@ -7,10 +7,12 @@ namespace Core
     {
         public int damageAmount = 20;
         [SerializeField] protected new BoxCollider2D collider;
+        AudioSource _audioSrc;
 
         private void Awake()
         {
             collider = GetComponent<BoxCollider2D>();
+            _audioSrc = GetComponent<AudioSource>();
         }
         
         void OnTriggerEnter2D(Collider2D collider) => HandleCollision2D(collider);
@@ -20,6 +22,7 @@ namespace Core
             var attackable = collider.TryGetComponent<IAttackable>(out var attackableComponent);
             if (!attackable) return;
             attackableComponent.TakeDamage(damageAmount);
+            _audioSrc.Play();
         }
     }
 }
